98f1f1b577b71da387a5541cad64703fac10856a
justinhxc <jubr5704@colorado.edu>
2016-04-17 21:25:38 -0600

    soon

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout<<"You begin your quest for fame and fortune!"<<endl;
+    cout<<"You heard rumor of treasure and glory to be had in the elemental dungeons"<<endl;
+    cout<<"You grab the gear your family owns which includes"<<endl;
     return 0;
 }
